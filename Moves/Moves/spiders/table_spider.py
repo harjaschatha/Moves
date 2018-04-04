98bb51b2ce61af570c82b4d5685a50a0ec0208c5
harjaschatha <harjaschatha@gmail.com>
2018-04-04 22:49:41 +0530

    Changed spider stuff

--- Moves/Moves/spiders/table_spider.py
+++ Moves/Moves/spiders/table_spider.py
@@ -1,27 +1,94 @@
 import scrapy
 
-
+MOVES = {}
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = 'pokenames'
+    start_urls = ['https://pokemondb.net/pokedex/national']
 
     def start_requests(self):
-        urls = [
-            'https://pokemondb.net/pokedex/bulbasaur'.
-        ]
-        
+        urls = self.start_urls
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url, self.parse_pokemons)
 
-    def parse(self, response):
-        products = response.xpath('//*[@id="svtabs_moves_16"]/div[1]/div[1]/div[1]/table/tbody//tr')
-    # ignore the table header row
-        for product in products[1:]:
-            
-            item = Schooldates1Item()
-            item['level'] = product.c.xpath('//*[@id="svtabs_moves_16"]/div[1]/div[1]/div[1]/table/tbody/tr[1]/td[1]//text()').extract_first()
-            item['move'] = product.c.xpath('//*[@id="svtabs_moves_16"]/div[1]/div[1]/div[1]/table/tbody/tr[1]/td[2]//text()').extract_first()
-            item['type'] = product.c.xpath('//*[@id="svtabs_moves_16"]/div[1]/div[1]/div[1]/table/tbody/tr[1]/td[3]//text()').extract_first()
-            item['category'] = product.c.xpath('//*[@id="svtabs_moves_16"]/div[1]/div[1]/div[1]/table/tbody/tr[1]/td[4]//text()').extract_first()
-            item['power'] = product.c.xpath('//*[@id="svtabs_moves_16"]/div[1]/div[1]/div[1]/table/tbody/tr[1]/td[5]//text()').extract_first()
-            item['level'] = product.c.xpath('//*[@id="svtabs_moves_16"]/div[1]/div[1]/div[1]/table/tbody/tr[1]/td[6]//text()').extract_first()
-            yield item
+    def parse_pokemons(self, response):
+        names = []
+        dex = []
+        pokemons = response.css('.infocard-tall-list .infocard-tall')
+        for pokemon in pokemons:
+            name = pokemon.css('.ent-name::text').extract_first()
+
+
+            names.append(name)
+        ### at this point, we will have a list of all pokemons names, called names
+
+        # STEP 2
+        
+        # we will create a new URL for each pokemon in the pokemons list
+        i = 1
+        base_url = "https://pokemondb.net/pokedex/"
+        for name in names[:251]:
+            if name == "Farfetch'd":
+                name = "farfetchd"
+            elif name == 'Mr. Mime':
+                name = "mr-mime"
+            elif name == 'Nidoran♂':
+                name = 'Nidoran-m'
+            elif name == 'Nidoran♀':
+                name = 'Nidoran-f'
+            elif name == 'Mime Jr.':
+                name = 'mime-jr'
+            elif name == 'Flabébé':
+                name = 'flabebe'
+            elif name == 'Type: Null':
+                name = 'type-null'
+            elif name == 'Tapu Koko':
+                name = 'tapu-koko'
+            elif name == 'Tapu Lele':
+                name = 'tapu-lele'
+            elif name == 'Tapu Bulu':
+                name = 'tapu-bulu'
+            elif name == 'Tapu Fini':
+                name = 'tapu-fini'
+
+            url = base_url + name.lower().replace(' ','')
+            yield scrapy.Request(url, self.parse_pokemon)
+    
+# MOVES = {'Bulbasaur': {moves_for_pokemon},"Ivysaur":},
+
+    def parse_pokemon(self, response):
+        MOVE_TYPES = ["Level", "Egg", "Tutor", "TM", "Name"]
+        all_moves = response.css(".wide-table")
+        ultra_series_moves = all_moves[:4]
+
+        name = response.xpath('/html/body/article/h1//text()').extract_first()
+
+        moves_for_pokemon = {}
+        for i, selector in enumerate(ultra_series_moves):
+            sub_moves = []
+            rows = selector.css("tbody tr")
+            for row in rows:
+                if i == 0:
+                    move_level = row.css("td.num::text").extract_first()
+                    move_name = row.css("td a.ent-name::text").extract_first()
+                    move = {'level': move_level, 'name': move_name}
+                elif i == 1:
+                    move_name = row.css("td a.ent-name::text").extract_first()
+                    move = {'name': move_name}
+                elif i == 2:
+                    move_name = row.css("td a.ent-name::text").extract_first()
+                    move = {'name': move_name}
+                else:
+                    # i == 3
+                    tm = row.css("td.num::text").extract_first()
+                    move_name = row.css("td a.ent-name::text").extract_first()
+                    move = {'tm': tm, 'name': move_name}
+                sub_moves.append(move)
+            moves_for_pokemon[MOVE_TYPES[i]] = sub_moves
+
+        MOVES[name] = moves_for_pokemon
+
+
+
+
+
+
